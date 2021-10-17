9be9dff694eb4c3958307d450eced0e7b4b6a36d
declan <syas0301@gmail.com>
2021-10-17 22:28:36 +0900

    make it better

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const server = httpServer.createServer(app);
 const io = require("socket.io")(server, {
     cors: {
-        origin: "http://localhost:1337",
+        origin: "/",
         methods: ["GET", "POST"],
     },
 });
